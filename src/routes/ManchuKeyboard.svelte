467ae188ba24d5dbbe8c5403c75cf0ebc3a16597
sj <64534668+sj-h4@users.noreply.github.com>
2023-05-18 23:16:18 +0900

    [wip] add juwan juwe uju 1

--- src/routes/ManchuKeyboard.svelte
+++ src/routes/ManchuKeyboard.svelte
@@ -1,48 +1,262 @@
 <script lang="ts">
-	import Key from "./Key.svelte";
+	import Key from './Key.svelte';
 </script>
 
 <div>
-	<div>Vowels</div>
+	<div>第一字頭</div>
 	<div class="manchu-keyboard-container">
-		 <Key on:clickedCharacter character="&#x1820" /> <!-- a -->
-		 <Key on:clickedCharacter character="&#x185d" /> <!-- e -->
-		 <Key on:clickedCharacter character="&#x1822" /> <!-- i -->
-		 <Key on:clickedCharacter character="&#x1824" /> <!-- o -->
-		 <Key on:clickedCharacter character="&#x1860" /> <!-- u -->
-		 <Key on:clickedCharacter character="&#x1861" /> <!-- v -->
+		<!-- a -->
+		<Key on:clickedCharacter character="&#x1820" />
+		<!-- e -->
+		<Key on:clickedCharacter character="&#x185d" />
+		<!-- i -->
+		<Key on:clickedCharacter character="&#x1822" />
+		<!-- o -->
+		<Key on:clickedCharacter character="&#x1824" />
+		<!-- u -->
+		<Key on:clickedCharacter character="&#x1860" />
+		<!-- v -->
+		<Key on:clickedCharacter character="&#x1861" />
+		<!-- na -->
+		<Key on:clickedCharacter character="&#x1828&#x1820" />
+		<!-- ne -->
+		<Key on:clickedCharacter character="&#x1828&#x185d" />
+		<!-- ni -->
+		<Key on:clickedCharacter character="&#x1828&#x1822" />
+		<!-- no -->
+		<Key on:clickedCharacter character="&#x1828&#x1824" />
+		<!-- nu -->
+		<Key on:clickedCharacter character="&#x1828&#x1860" />
+		<!-- nv -->
+		<Key on:clickedCharacter character="&#x1828&#x1861" />
+		<!-- ka -->
+		<Key on:clickedCharacter character="&#x1874&#x1820" />
+		<!-- ga -->
+		<Key on:clickedCharacter character="&#x1864&#x1820" />
+		<!-- ha -->
+		<Key on:clickedCharacter character="&#x1865&#x1820" />
+		<!-- ko -->
+		<Key on:clickedCharacter character="&#x1874&#x1824" />
+		<!-- go -->
+		<Key on:clickedCharacter character="&#x1864&#x1824" />
+		<!-- ho -->
+		<Key on:clickedCharacter character="&#x1865&#x1824" />
+		<!-- kv -->
+		<Key on:clickedCharacter character="&#x1874&#x1861" />
+		<!-- gv -->
+		<Key on:clickedCharacter character="&#x1864&#x1861" />
+		<!-- hv -->
+		<Key on:clickedCharacter character="&#x1865&#x1861" />
+		<!-- ba -->
+		<Key on:clickedCharacter character="&#x182a&#x1820" />
+		<!-- be -->
+		<Key on:clickedCharacter character="&#x182a&#x185d" />
+		<!-- bi -->
+		<Key on:clickedCharacter character="&#x182a&#x1822" />
+		<!-- bo -->
+		<Key on:clickedCharacter character="&#x182a&#x1824" />
+		<!-- bu -->
+		<Key on:clickedCharacter character="&#x182a&#x1860" />
+		<!-- bv -->
+		<Key on:clickedCharacter character="&#x182a&#x1861" />
+		<!-- pa -->
+		<Key on:clickedCharacter character="&#x182b&#x1820" />
+		<!-- pe -->
+		<Key on:clickedCharacter character="&#x182b&#x185d" />
+		<!-- pi -->
+		<Key on:clickedCharacter character="&#x182b&#x1822" />
+		<!-- po -->
+		<Key on:clickedCharacter character="&#x182b&#x1824" />
+		<!-- pu -->
+		<Key on:clickedCharacter character="&#x182b&#x1860" />
+		<!-- pv -->
+		<Key on:clickedCharacter character="&#x182b&#x1861" />
+		<!-- sa -->
+		<Key on:clickedCharacter character="&#x1830&#x1820" />
+		<!-- se -->
+		<Key on:clickedCharacter character="&#x1830&#x185d" />
+		<!-- si -->
+		<Key on:clickedCharacter character="&#x1830&#x1822" />
+		<!-- so -->
+		<Key on:clickedCharacter character="&#x1830&#x1824" />
+		<!-- su -->
+		<Key on:clickedCharacter character="&#x1830&#x1860" />
+		<!-- sv -->
+		<Key on:clickedCharacter character="&#x1830&#x1861" />
+		<!-- xa -->
+		<Key on:clickedCharacter character="&#x1867&#x1820" />
+		<!-- xe -->
+		<Key on:clickedCharacter character="&#x1867&#x185d" />
+		<!-- xi -->
+		<Key on:clickedCharacter character="&#x1867&#x1822" />
+		<!-- xo -->
+		<Key on:clickedCharacter character="&#x1867&#x1824" />
+		<!-- xu -->
+		<Key on:clickedCharacter character="&#x1867&#x1860" />
+		<!-- xv -->
+		<Key on:clickedCharacter character="&#x1867&#x1861" />
+		<!-- ta -->
+		<Key on:clickedCharacter character="&#x1832&#x1820" />
+		<!-- -->
+		<Key on:clickedCharacter character="" />
+		<!-- da -->
+		<Key on:clickedCharacter character="&#x1869&#x1820" />
+		<!-- te -->
+		<Key on:clickedCharacter character="&#x1832&#x185d" />
+		<!-- -->
+		<Key on:clickedCharacter character="" />
+		<!-- de -->
+		<Key on:clickedCharacter character="&#x1869&#x185d" />
+		<!-- ti -->
+		<Key on:clickedCharacter character="&#x1832&#x1822" />
+		<!-- -->
+		<Key on:clickedCharacter character="" />
+		<!-- di -->
+		<Key on:clickedCharacter character="&#x1869&#x1822" />
+		<!-- to -->
+		<Key on:clickedCharacter character="&#x1832&#x1824" />
+		<!-- -->
+		<Key on:clickedCharacter character="" />
+		<!-- do -->
+		<Key on:clickedCharacter character="&#x1869&#x1824" />
+		<!-- tu -->
+		<Key on:clickedCharacter character="&#x1832&#x1860" />
+		<!-- -->
+		<Key on:clickedCharacter character="" />
+		<!-- du -->
+		<Key on:clickedCharacter character="&#x1869&#x1860" />
+		<!-- la -->
+		<Key on:clickedCharacter character="&#x182f&#x1820" />
+		<!-- le -->
+		<Key on:clickedCharacter character="&#x182f&#x185d" />
+		<!-- li -->
+		<Key on:clickedCharacter character="&#x182f&#x1822" />
+		<!-- lo -->
+		<Key on:clickedCharacter character="&#x182f&#x1824" />
+		<!-- lu -->
+		<Key on:clickedCharacter character="&#x182f&#x1860" />
+		<!-- lv -->
+		<Key on:clickedCharacter character="&#x182f&#x1861" />
+		<!-- ma -->
+		<Key on:clickedCharacter character="&#x182e&#x1820" />
+		<!-- me -->
+		<Key on:clickedCharacter character="&#x182e&#x185d" />
+		<!-- mi -->
+		<Key on:clickedCharacter character="&#x182e&#x1822" />
+		<!-- mo -->
+		<Key on:clickedCharacter character="&#x182e&#x1824" />
+		<!-- mu -->
+		<Key on:clickedCharacter character="&#x182e&#x1860" />
+		<!-- mv -->
+		<Key on:clickedCharacter character="&#x182e&#x1861" />
+		<!-- ca -->
+		<Key on:clickedCharacter character="&#x1834&#x1820" />
+		<!-- ce -->
+		<Key on:clickedCharacter character="&#x1834&#x185d" />
+		<!-- ci -->
+		<Key on:clickedCharacter character="&#x1834&#x1822" />
+		<!-- co -->
+		<Key on:clickedCharacter character="&#x1834&#x1824" />
+		<!-- cu -->
+		<Key on:clickedCharacter character="&#x1834&#x1860" />
+		<!-- cv -->
+		<Key on:clickedCharacter character="&#x1834&#x1861" />
+		<!-- ja -->
+		<Key on:clickedCharacter character="&#x1835&#x1820" />
+		<!-- je -->
+		<Key on:clickedCharacter character="&#x1835&#x185d" />
+		<!-- ji -->
+		<Key on:clickedCharacter character="&#x1835&#x1822" />
+		<!-- jo -->
+		<Key on:clickedCharacter character="&#x1835&#x1824" />
+		<!-- ju -->
+		<Key on:clickedCharacter character="&#x1835&#x1860" />
+		<!-- jv -->
+		<Key on:clickedCharacter character="&#x1835&#x1861" />
+		<!-- ya -->
+		<Key on:clickedCharacter character="&#x182d&#x1820" />
+		<!-- -->
+		<Key on:clickedCharacter character="" />
+		<!-- ye -->
+		<Key on:clickedCharacter character="&#x182d&#x185d" />
+		<!-- yo -->
+		<Key on:clickedCharacter character="&#x182d&#x1824" />
+		<!-- yu -->
+		<Key on:clickedCharacter character="&#x182d&#x1860" />
+		<!-- yv -->
+		<Key on:clickedCharacter character="&#x182d&#x1861" />
+		<!-- ke -->
+		<Key on:clickedCharacter character="&#x1874&#x185d" />
+		<!-- ge -->
+		<Key on:clickedCharacter character="&#x1864&#x185d" />
+		<!-- he -->
+		<Key on:clickedCharacter character="&#x1865&#x185d" />
+		<!-- ki -->
+		<Key on:clickedCharacter character="&#x1874&#x1822" />
+		<!-- gi -->
+		<Key on:clickedCharacter character="&#x1864&#x1822" />
+		<!-- hi -->
+		<Key on:clickedCharacter character="&#x1865&#x1822" />
+		<!-- ku -->
+		<Key on:clickedCharacter character="&#x1874&#x1860" />
+		<!-- gu -->
+		<Key on:clickedCharacter character="&#x1864&#x1860" />
+		<!-- hu -->
+		<Key on:clickedCharacter character="&#x1865&#x1860" />
 	</div>
 	<div>Consonatns</div>
 	<div class="manchu-keyboard-container">
-		<Key on:clickedCharacter character="&#x1828" /> <!-- n -->
-		<Key on:clickedCharacter character="&#x1829" /> <!-- ng -->
-		<Key on:clickedCharacter character="&#x182a" /> <!-- b -->
-		<Key on:clickedCharacter character="&#x182b" /> <!-- p -->
-		<Key on:clickedCharacter character="&#x182c" /> <!-- q -->
-		<Key on:clickedCharacter character="&#x182d" /> <!-- G -->
-		<Key on:clickedCharacter character="&#x182e" /> <!-- m -->
-		<Key on:clickedCharacter character="&#x182f" /> <!-- l -->
-		<Key on:clickedCharacter character="&#x1830" /> <!-- s -->
-		<Key on:clickedCharacter character="&#x1867" /> <!-- x -->
-		<Key on:clickedCharacter character="&#x1832" /> <!-- t -->
-		<Key on:clickedCharacter character="&#x1869" /> <!-- d -->
-		<Key on:clickedCharacter character="&#x1834" /> <!-- c -->
-		<Key on:clickedCharacter character="&#x1835" /> <!-- j -->
-		<Key on:clickedCharacter character="&#x1836" /> <!-- y -->
-		<Key on:clickedCharacter character="&#x1837" /> <!-- r -->
-		<Key on:clickedCharacter character="&#x1838" /> <!-- w -->
-		<Key on:clickedCharacter character="&#x1839" /> <!-- f -->
-		<Key on:clickedCharacter character="&#x1874" /> <!-- k -->
-		<Key on:clickedCharacter character="&#x1864" /> <!-- g -->
-		<Key on:clickedCharacter character="&#x1865" /> <!-- h -->
+		<Key on:clickedCharacter character="&#x1828" />
+		<!-- n -->
+		<Key on:clickedCharacter character="&#x1829" />
+		<!-- ng -->
+		<Key on:clickedCharacter character="&#x182a" />
+		<!-- b -->
+		<Key on:clickedCharacter character="&#x182b" />
+		<!-- p -->
+		<Key on:clickedCharacter character="&#x182c" />
+		<!-- q -->
+		<Key on:clickedCharacter character="&#x182d" />
+		<!-- G -->
+		<Key on:clickedCharacter character="&#x182e" />
+		<!-- m -->
+		<Key on:clickedCharacter character="&#x182f" />
+		<!-- l -->
+		<Key on:clickedCharacter character="&#x1830" />
+		<!-- s -->
+		<Key on:clickedCharacter character="&#x1867" />
+		<!-- x -->
+		<Key on:clickedCharacter character="&#x1832" />
+		<!-- t -->
+		<Key on:clickedCharacter character="&#x1869" />
+		<!-- d -->
+		<Key on:clickedCharacter character="&#x1834" />
+		<!-- c -->
+		<Key on:clickedCharacter character="&#x1835" />
+		<!-- j -->
+		<Key on:clickedCharacter character="&#x1836" />
+		<!-- y -->
+		<Key on:clickedCharacter character="&#x1837" />
+		<!-- r -->
+		<Key on:clickedCharacter character="&#x1838" />
+		<!-- w -->
+		<Key on:clickedCharacter character="&#x1839" />
+		<!-- f -->
+		<Key on:clickedCharacter character="&#x1874" />
+		<!-- k -->
+		<Key on:clickedCharacter character="&#x1864" />
+		<!-- g -->
+		<Key on:clickedCharacter character="&#x1865" />
+		<!-- h -->
 	</div>
 </div>
 
 <style lang="scss">
 	.manchu-keyboard-container {
 		display: grid;
-		grid-template-columns: repeat(6, 1fr);
-		grid-template-rows: repeat(8, 1fr);
+		grid-auto-flow: column;
+		grid-template-columns: repeat(13, 1fr);
+		grid-template-rows: repeat(9, 1fr);
 		gap: 5px;
 	}
 </style>
